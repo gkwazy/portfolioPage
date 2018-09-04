865706051d6f7064f1d7f3b9fbbc957eb65fa588
gkwazy <noreply>
2018-09-04 14:34:22 -0600

    page only

---
+++ public/template/mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
+	$to = 'gkwazy@gmail.com'; // Enter Your E-mail
+	$from = 'admin@gkwazy.com'; // Admin e-mail
+	$fromName = 'Your Company Name'; // Your company name
+	$charset = 'UTF-8';
+?>
